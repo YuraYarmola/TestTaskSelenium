bd680b3e17b8c8efbe97c4b5a31610178f8107f0
YuraYarmola <uarmola1@gmail.com>
2024-07-26 10:50:15 +0300

    try run on robocorp

--- try_robocorp.py
+++ try_robocorp.py
@@ -1,11 +1,12 @@
-from robocorp.tasks import task
-from robocorp.tasks import get_output_dir
 from RPA.Browser.Selenium import Selenium
-from selenium.webdriver.common.keys import Keys
 
-@task
-def minimal_task():
-  browser = Selenium(auto_close = False)
-  browser.open_available_browser('https://www.google.co.uk')
-  browser.click_button('Accept all')
-  browser.input_text('name:q','rpaframework' +Keys.ENTER)
+
+def open_google():
+    browser = Selenium()
+    browser.open_available_browser("https://www.google.com")
+    browser.screenshot("google_homepage.png")
+    browser.close_all_browsers()
+
+
+if __name__ == "__main__":
+    open_google()
